df1f7bb08dbb3e02de3b4f25d83f861f1d110fdf
Pengwwwin <ruyguz7@gmail.com>
2021-09-02 13:39:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace webCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            webCalendarApp calendarAPP = new webCalendarApp();
+            User u = calendarAPP->getUserList()->searchById(123); // return a user.
+            Calendar c= u->getCalendarList()->getCalendarById(333); // return calendar of the previous user.
+            c->printReminders(DateTime d); //show some  reminders of the previous calendar.
+            c->printReminders(string* tags[]) //show some reminders by a given tag.
+
+
+        }
+    }
+}
---
+++ webCalendarApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webCalendar
+{
+    class webCalendarApp
+    {
+        public int Id { get; set; }
+
+        
+        //UsersCollection
+        //
+        //CalendarCollection
+        //constructor
+        //delete
+        //addNewCalendar
+        //addNewUser
+        //
+
+
+    }
+}
